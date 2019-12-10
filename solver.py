9f00f19e2e5490f3c83844ebc1965f6bcd2506ee
iojea <iojea@dm.uba.ar>
2019-12-10 16:55:08 -0300

    First commit. Skeleton of file.

---
+++ solver.py
@@ -0,0 +1,21 @@
+# Assembler: 
+
+local_stiffness = { 4 : local_stiffness_tetra,
+					5 : local_stiffness_pyramid,
+					6 : local_stiffness_prism}
+
+def load_mesh():
+	pass
+
+def assembler():
+	# loop over elements and assemble stiffness matrix
+	pass
+
+def local_stiffness_tetra():
+	pass
+
+def local_stiffness_prism():
+	pass
+
+def local_stiffness_pyramid():
+	pass
