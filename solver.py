b2cb2ddc5b8ffbf10d39d6536deba85a4dc2d879
alexisjawtu <ajawtu@dm.uba.ar>
2020-01-03 13:09:44 -0300

    IMPORTANT: changed TABS into FOUR spaces

--- solver.py
+++ solver.py
@@ -2,11 +2,11 @@
 import numpy as np
 
 local_stiffness = { 4 : local_stiffness_tetra,
-					5 : local_stiffness_pyramid,
-					6 : local_stiffness_prism}
+                    5 : local_stiffness_pyramid,
+                    6 : local_stiffness_prism}
 
 def load_mesh(in_file):
-	pass
+    pass
     vertices = np.genfromtxt(in_file+".ver").T
     with open (in_file+."ebv") as infile:
         inlist = infile.readlines()
@@ -17,25 +17,25 @@ def load_mesh(in_file):
     elements = [np.array([int(st[k]) for k in range(len(st))]) for st in pre_list]
 
 def assembler():
-	# loop over elements and assemble stiffness matrix
-	pass
+    # loop over elements and assemble stiffness matrix
+    pass
 
 def local_stiffness_tetra(p):
-	"""
+    """
     Input:  3x4 matrix, vertices of tetrahedron.
-	Output: a vector with the coefficients of the local
-	        stiffness matrix. 
+    Output: a vector with the coefficients of the local
+            stiffness matrix. 
     """ 
-	d = p.shape[1]
+    d = p.shape[1]
     stiff_rhs = np.vstack((np.zeros((1,d)),np.eye(d)))
-	H = np.vstack((np.ones((1,d+1)),nodes.T))
+    H = np.vstack((np.ones((1,d+1)),nodes.T))
     meas_t = np.absolute(np.linalg.det(H))/factorial(d)
     G = np.linalg.solve(H,stiff_rhs)
     stiffness_loc = meas_t*G@G.T
     return stiffness_loc.flatten()
         
 def local_stiffness_prism():
-	pass
+    pass
 
 def local_stiffness_pyramid():
-	pass
+    pass
